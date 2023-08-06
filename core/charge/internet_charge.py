db6288fba5d37f310a1bbce15f6a4cdc8c9c8daf
Vahab Rasti <vahab.rasti@gmail.com>
2023-08-06 20:29:56 +1000

    fix(float): decimal to float

--- core/charge/internet_charge.py
+++ core/charge/internet_charge.py
@@ -89,14 +89,16 @@ class InternetCharge(ChargeWithRules):
             calculate and return amount of credit that this instance of user consumed
             during --EFFECTIVE-- rule only
         """
-        now=time.time()
-        effective_rule=user_obj.charge_info.effective_rules[instance-1]
-        timely_credit_used=0
-        transfer_credit_used=0
-        if effective_rule.cpm>0:
-            timely_credit_used=effective_rule.cpm * (now - user_obj.charge_info.rule_start[instance-1]) / 60
-        if effective_rule.cpk>0:
-            transfer_credit_used=effective_rule.cpk * (effective_rule.calcRuleTransferUsage(user_obj,instance)) / 1024.0
+        now = time.time()
+        effective_rule = user_obj.charge_info.effective_rules[instance - 1]
+        timely_credit_used = 0.0
+        transfer_credit_used = 0.0
+
+        if effective_rule.cpm > 0:
+            timely_credit_used = effective_rule.cpm * (now - user_obj.charge_info.rule_start[instance - 1]) / 60.0
+
+        if effective_rule.cpk > 0:
+            transfer_credit_used = effective_rule.cpk * effective_rule.calcRuleTransferUsage(user_obj,instance) / 1024.0
 
         if CHARGE_DEBUG:
             toLog("user_id: %s timely_credit_used: %s transfer_credit_used: %s"%(user_obj.getUserID(),timely_credit_used,transfer_credit_used),LOG_DEBUG)
