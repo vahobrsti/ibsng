b997be147d3c5bcbbb8aae209a9804836d007dfb
vahobrsti <vahab.rasti@gmail.com>
2023-12-25 22:25:54 +1100

    feat(migration7): initial phase for migrating interface/IBSng/inc/generator/generator_controller.php

--- interface/IBSng/inc/generator/generator_controller.php
+++ interface/IBSng/inc/generator/generator_controller.php
@@ -1,99 +1,110 @@
 <?php
 
-
 /**
- * 
- * */
-
+ * Class GeneratorController
+ */
 class GeneratorController
-	{
-    function GeneratorController()
+{
+    protected $smarty;
+    protected $creator;
+    protected $generator;
+    protected $__generators = [];
+
+    /**
+     * GeneratorController constructor.
+     */
+    public function __construct()
     {
         $this->init();
         $this->smarty = new IBSSmarty();
-		$this->creator = $this->getCreator();
-		$this->creator->registerController($this);
-		$this->generator = $this->getGenerator();
-    }
-
-    function init()
-    {
-        $this->__generators = array ();
+        $this->creator = $this->getCreator();
+        $this->creator->registerController($this);
+        $this->generator = $this->getGenerator();
     }
 
     /**
-     * display output view
-     * */
-    function display()
-	{
-    	$this->creator->create();
+     * Initialize generators array.
+     */
+    protected function init()
+    {
+        $this->__generators = [];
     }
-
-	/**
-	 * get report result and needed variable from it.
-	 * */
-	function extractVarsFromResults (& $results)
-	{
-	}
-
-	/**
-	 * abstract function getCreator()
-	 * TODO: you must overide this function
-	 * @return Creator
-	 * */
-	function getCreator ()
-	{
-	    return NULL;
-	}
-
-	/**
-	 * abstract function getGenerator()
-	 * TODO: you must overide this function
-	 * @return Generator
-	 * */
-	function getGenerator ()
-	{
-	    return NULL;
-	}
-
-	/**
-	 * for any variable of REQUEST if it start with $selector_expression then is added to return array.
-	 * example :
-	 * 		getReportSelectors("show__") return an array that contains this variables 
-	 * 		"show__details_username", "show__derails_userid", ... that this variables come from
-	 * 		REQUEST.
-	 * 
-	 * @param string $begin_expression like this : show__details_ each var that start with this 
-	 * 		expression will be selected 
-	 * @return array 
-	 * */
-	function getReportSelectors($begin_expression = "show__") {
-		$selectors = array ();
-		$search_replace = array ("_" => " ", "SLASH" => "/");
-
-		// searching in REQUEST and find selectors each selector start with $selector_start
-		foreach ($_REQUEST as $field_name => $field_formula)
-			if (ereg("^" . $begin_expression . ".*", $field_formula))
-				$selectors[str_replace( array_keys($search_replace),
-						  	array_values($search_replace),
-						  	$field_name)] = $field_formula;
-
-		return $selectors;
-	}
 
     /**
-     * map $generator_name with it's function
-     * @param $generator_name string name of generator (WEB for example)
-     * @param $generator_function string function name (createWebGenerator for example)
-     * */
-    function registerGenerator($generator_name, $generator_function)
+     * Display output view.
+     */
+    public function display()
     {
-        $this->__generators = array_merge(array($generator_name => $generator_function), $this->__generators);
+        $this->creator->create();
     }
-	
-	function getGenerators ()
-	{
-	    return $this->__generators;
-	}
+
+    /**
+     * Get report result and needed variables from it.
+     *
+     * @param array $results
+     */
+    public function extractVarsFromResults(&$results)
+    {
+    }
+
+    /**
+     * Abstract function to get Creator.
+     *
+     * @return Creator|null
+     */
+    protected function getCreator()
+    {
+        return null;
+    }
+
+    /**
+     * Abstract function to get Generator.
+     *
+     * @return Generator|null
+     */
+    protected function getGenerator()
+    {
+        return null;
+    }
+
+    /**
+     * Get report selectors.
+     *
+     * @param string $beginExpression
+     * @return array
+     */
+    public function getReportSelectors($beginExpression = "show__")
+    {
+        $selectors = [];
+        $searchReplace = ["_" => " ", "SLASH" => "/"];
+
+        foreach ($_REQUEST as $fieldName => $fieldFormula) {
+            if (preg_match("/^" . $beginExpression . ".*/", $fieldFormula)) {
+                $selectors[str_replace(array_keys($searchReplace), array_values($searchReplace), $fieldName)] = $fieldFormula;
+            }
+        }
+
+        return $selectors;
+    }
+
+    /**
+     * Register a generator.
+     *
+     * @param string $generatorName
+     * @param string $generatorFunction
+     */
+    public function registerGenerator($generatorName, $generatorFunction)
+    {
+        $this->__generators = array_merge([$generatorName => $generatorFunction], $this->__generators);
+    }
+
+    /**
+     * Get registered generators.
+     *
+     * @return array
+     */
+    public function getGenerators()
+    {
+        return $this->__generators;
+    }
 }
-?>
