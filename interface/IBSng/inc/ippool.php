cde9324363e05468b8bfef61bf6e160f16b559d7
vahobrsti <vahab.rasti@gmail.com>
2023-12-25 22:07:28 +1100

    feat(migration7): initial phase for migrating interface/IBSng/inc/ippool.php

--- interface/IBSng/inc/ippool.php
+++ interface/IBSng/inc/ippool.php
@@ -3,89 +3,98 @@ require_once("init.php");
 
 class AddNewIPpool extends Request
 {
-    function AddNewIPpool($ippool_name,$comment)
+    public function __construct($ippool_name, $comment)
     {
-        parent::Request("ippool.addNewIPpool",array("ippool_name"=>$ippool_name,
-                                                    "comment"=>$comment));
+        parent::__construct("ippool.addNewIPpool", [
+            "ippool_name" => $ippool_name,
+            "comment" => $comment,
+        ]);
     }
 }
 
 class UpdateIPpool extends Request
 {
-    function UpdateIPpool($ippool_id,$ippool_name,$comment)
+    public function __construct($ippool_id, $ippool_name, $comment)
     {
-        parent::Request("ippool.updateIPpool",array("ippool_id"=>$ippool_id,
-                                                    "ippool_name"=>$ippool_name,
-                                                    "comment"=>$comment));
+        parent::__construct("ippool.updateIPpool", [
+            "ippool_id" => $ippool_id,
+            "ippool_name" => $ippool_name,
+            "comment" => $comment,
+        ]);
     }
 }
 
 class GetIPpoolNames extends Request
 {
-    function GetIPpoolNames()
+    public function __construct()
     {
-        parent::Request("ippool.getIPpoolNames",array());
+        parent::__construct("ippool.getIPpoolNames", []);
     }
 }
 
 class GetIPpoolInfo extends Request
 {
-    function GetIPpoolInfo($ippool_name)
+    public function __construct($ippool_name)
     {
-        parent::Request("ippool.getIPpoolInfo",array("ippool_name"=>$ippool_name));
+        parent::__construct("ippool.getIPpoolInfo", [
+            "ippool_name" => $ippool_name,
+        ]);
     }
 }
 
 class DeleteIPpool extends Request
 {
-    function DeleteIPpool($ippool_name)
+    public function __construct($ippool_name)
     {
-        parent::Request("ippool.deleteIPpool",array("ippool_name"=>$ippool_name));
+        parent::__construct("ippool.deleteIPpool", [
+            "ippool_name" => $ippool_name,
+        ]);
     }
 }
 
 class DelIPfromPool extends Request
 {
-    function DelIPfromPool($ippool_name,$ip)
+    public function __construct($ippool_name, $ip)
     {
-        parent::Request("ippool.delIPfromPool",array("ippool_name"=>$ippool_name,
-                                                     "ip"=>$ip));
+        parent::__construct("ippool.delIPfromPool", [
+            "ippool_name" => $ippool_name,
+            "ip" => $ip,
+        ]);
     }
 }
 
 class AddIPtoPool extends Request
 {
-    function AddIPtoPool($ippool_name,$ip)
+    public function __construct($ippool_name, $ip)
     {
-        parent::Request("ippool.addIPtoPool",array("ippool_name"=>$ippool_name,
-                                                   "ip"=>$ip));
+        parent::__construct("ippool.addIPtoPool", [
+            "ippool_name" => $ippool_name,
+            "ip" => $ip,
+        ]);
     }
 }
-
 function getIPpoolInfos()
 {/*    
     Return an array of all ip pool infos in format (ippool_name=>associative_ippool_info_array)
  */
     $ippool_list_req=new GetIPpoolNames();
     list($success,$ippool_names)=$ippool_list_req->send();
-    if(!$success)
-        return array(FALSE,$ippool_names);
-    else
-    {
+    if(!$success) {
+        return array(FALSE, $ippool_names);
+    }else {
         $ippools_info=array();
         $ippool_info_req=new GetIPpoolInfo("");
         foreach($ippool_names as $ippool_name)
         {
             $ippool_info_req->changeParam("ippool_name",$ippool_name);
             list($success,$ippool_info)=$ippool_info_req->send();
-            if(!$success)
-                return array(FALSE,$ippool_info);
-            else
-                $ippools_info[$ippool_name]=$ippool_info;
+            if(!$success) {
+                return [false,$ippool_info];
+            }
+            else {
+                $ippools_info[$ippool_name] = $ippool_info;
+            }
         }
-        return array(TRUE,$ippools_info);
+        return [true,$ippools_info];
     }
 }
-
-
-?>
